2ee8e76fb3e85ef2ed32ebc9e5c7c2029ddae086
AndryMorgan <rafanoharanamorgan4@gmail.com>
2026-01-16 13:38:02 +0300

    add the enum and the db.sql

---
+++ src/main/java/EnumForIngredient/category.java
@@ -0,0 +1,9 @@
+package EnumForIngredient;
+
+public enum category {
+        VEGETABLE,
+        ANIMAL,
+        MARINE,
+        DIARY,
+        OTHER
+}
---
+++ src/main/java/EnumForIngredient/dish_type.java
@@ -0,0 +1,7 @@
+package EnumForIngredient;
+
+public enum dish_type {
+    STAR,
+    MAIN,
+    DESSERT
+}
