f7d83fe1897f255755b36a5a2a3e2c46c1c86956
AndryMorgan <rafanoharanamorgan4@gmail.com>
2026-01-19 13:53:42 +0300

    add the db.sql ALL_GRANT

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -2,4 +2,9 @@ create database "mini_dish_db";
 
 create user "mini_dish_db_manager" with password '123456';
 
--- Grant all privileges
+GRANT USAGE ON SCHEMA public TO mini_dish_db_manager;
+GRANT CREATE ON SCHEMA public TO mini_dish_db_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC
+      GRANT SELECT , INSERT , UPDATE , DELETE ON TABLES TO mini_dish_db_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC
+    GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO mini_dish_db_manager;
